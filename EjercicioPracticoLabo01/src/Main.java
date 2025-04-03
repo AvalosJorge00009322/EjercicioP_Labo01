6146be335fabff2bf1f4799682796a93547fd1bb
JuanPalaci <00057522@uca.edu.sv>
2025-04-03 10:30:06 -0600

    Adding add-doctor and main Menu

--- EjercicioPracticoLabo01/src/Main.java
+++ EjercicioPracticoLabo01/src/Main.java
@@ -1,43 +1,13 @@
-import Model.DTO.DoctorDTO;
-import Model.Entity.Doctor;
-import Model.Service.DoctorService;
-
-import java.util.List;
-
+import Model.Service.MainMenu;
 
 public class Main {
     public static void main(String[] args) {
-
-        DoctorDTO doctorDTO = new DoctorDTO(
-                "John",
-                "Doe",
-                "12345678-9",
-                new java.util.Date(),
-                "Cardiology",
-                new java.util.Date(),
-                "EMP001"
-        );
-
-        DoctorService doctorService = new DoctorService();
-        Doctor doctor = doctorService.createDoctor(doctorDTO);
-
-        List<Doctor> doctorList = List.of(doctor);
-        List<DoctorDTO> doctorDTOS = doctorList.stream().map(doctorItem -> {
-            return new DoctorDTO(
-                    doctorItem.getName(),
-                    doctorItem.getLastName(),
-                    doctorItem.getDUI(),
-                    doctorItem.getBirthday(),
-                    doctorItem.getSpeciality(),
-                    doctorItem.getDateOfRecruitment(),
-                    doctorItem.getEmployeeCode()
-            );
-        }).toList();
-
-        doctorDTOS.forEach(doctorItem -> {
-            System.out.println(doctorItem.getName() + " " + doctorItem.getLastName() + " " + doctorItem.getDUI());
-        });
-
-
+        MainMenu mainMenu = new MainMenu();
+        mainMenu.MainMenu();
     }
+
 }
+
+
+
+
